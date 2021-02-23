a42bb0841d3e586f3ae254371c01d40d828e2415
toyowata <toyomasa.watarai@arm.com>
2021-02-23 23:56:24 +0900

    Update library

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include "mbed.h"
 
 DigitalOut led1(LED1);
-Serial pc(USBTX, USBRX);
 SPI _spi(p5, p6, p7); // mosi, miso, sclk
 DigitalOut _cs(p8);
 DigitalOut _reset(p9);
@@ -24,20 +23,20 @@ void if_s_write(int addr,int data){
 
 void init_825(void) {
     _reset = 0;
-    wait_ms(1);
+    thread_sleep_for(1);
     _reset = 1;
 
     if_s_write( 0x1D, OUTPUT_power );
     if_s_write( 0x02, 0x0E );
-    wait_ms(1);
+    thread_sleep_for(1);
     if_s_write( 0x00, 0x01 ); //CLKEN
     if_s_write( 0x01, 0x00 ); //AKRST
     if_s_write( 0x1A, 0xA3 );
-    wait_ms(1);
+    thread_sleep_for(1);
     if_s_write( 0x1A, 0x00 );
-    wait_ms(30);
+    thread_sleep_for(30);
     if_s_write( 0x02, 0x04 ); //AP1,AP3
-    wait_ms(1);
+    thread_sleep_for(1);
     if_s_write( 0x02, 0x00 );
     //add
     if_s_write( 0x19, 0xF0 ); //MASTER VOL
@@ -46,7 +45,7 @@ void init_825(void) {
     if_s_write( 0x03, 0x01 ); //Analog Gain
    
     if_s_write( 0x08, 0xF6 );
-    wait_ms(21);
+    thread_sleep_for(21);
     if_s_write( 0x08, 0x00 );
     if_s_write( 0x09, 0xF8 );
     if_s_write( 0x0A, 0x00 );
@@ -69,7 +68,7 @@ void set_tone(void){
     };
   
     if_s_write( 0x08, 0xF6 );
-    wait_ms(1);
+    thread_sleep_for(1);
     if_s_write( 0x08, 0x00 );
   
     if_write( 0x07, &tone_data[0], 35 );//write to FIFO
@@ -97,8 +96,9 @@ void keyoff(void){
 
 // main() runs in its own thread in the OS
 int main() {
-    pc.baud(115200);
-    pc.printf("\nYMF826 test program\n");
+    printf("Mbed OS version %d.%d.%d\n\n", MBED_MAJOR_VERSION, MBED_MINOR_VERSION, MBED_PATCH_VERSION);
+    printf("YMF825 test program\n");
+    
     _cs = 1;
     _spi.format(8, 3);
     _spi.frequency(12000000);
@@ -110,25 +110,25 @@ int main() {
     while (true) {
         led1 = !led1;
         keyon(0x14,0x65);
-        wait_ms(500);
+        thread_sleep_for(500);
         keyoff();
-        wait_ms(200);
+        thread_sleep_for(200);
         keyon(0x1c,0x11);
-        wait_ms(500);
+        thread_sleep_for(500);
         keyoff();
-        wait_ms(200);
+        thread_sleep_for(200);
         keyon(0x1c,0x42);
-        wait_ms(500);
+        thread_sleep_for(500);
         keyoff();
-        wait_ms(200);
+        thread_sleep_for(200);
         keyon(0x1c,0x5d);
-        wait_ms(500);
+        thread_sleep_for(500);
         keyoff();
-        wait_ms(200);
+        thread_sleep_for(200);
         keyon(0x24,0x17);
-        wait_ms(500);
+        thread_sleep_for(500);
         keyoff();
-        wait_ms(200);
+        thread_sleep_for(200);
     }
 }
 
